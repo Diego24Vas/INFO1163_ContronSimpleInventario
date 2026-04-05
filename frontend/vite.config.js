c9f810593b4cd86513e87d84293bfea785767805
Diego Vasquez <168310676+Diego24Vas@users.noreply.github.com>
2026-04-04 22:26:50 -0300

    Se modifica para uso dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true, // Habilita el uso de polling para entornos como Docker o WSL
+    },
+    host: true, // Permite acceder desde la red local
+    port: 3000, // Configura el puerto del servidor
+  },
 })
